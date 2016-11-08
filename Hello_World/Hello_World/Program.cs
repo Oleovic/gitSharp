f3ce15c62736b06a09fad5c8dfbe012ea8f5b66b
OlgaV <oleovic@rambler.ru>
2016-11-08 21:40:17 +0500

    something else

--- Hello_World/Hello_World/Program.cs
+++ Hello_World/Hello_World/Program.cs
@@ -9,6 +9,7 @@ namespace Hello_World
     class Program
     {
         static void Main(string[] args)
+
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Good bye!");
